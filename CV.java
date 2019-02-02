e219b8079455018396601d95833abc95c99c1cac
amina <1772662@cmontmorency.qc.ca>
2019-02-02 12:53:49 -0500

    Creation de la premiere version du fichier CV.java

--- CV.java
+++ CV.java
@@ -68,24 +68,5 @@ public class CV {
 		return this.attentes;
 	}
 	// fin accesseur
-	// Methode qui permet d’afficher toutes les informations d’un CV.
-		public void affiche() {
 
-			String comptetencesList = "";
-
-			for (int i = 0; i < getCompetences().length; i++) {
-				if (getCompetences()[i] != null)
-					comptetencesList += "" + getCompetences()[i].toString() + ";";
-			}
-
-			String informations = "\nNom                             : " + getNom() + ""
-					+ "\nPrenom                          : " + getPrenom() + "\nFormation                       : "
-					+ getFormation() + "\nAnnée(s) expérience de travail  : " + getNbAnnees()
-					+ "\nCompetences                     : " + comptetencesList + "\nAttentes vis à vis le cours 4B4 : "
-					+ getAttentes();
-			System.out.println(informations);
-
-		}
-
-	
 }
